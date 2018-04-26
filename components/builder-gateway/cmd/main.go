b5bd5f09200a954991e4f26c8220a114235fa9a1
Elliott Davis <elliott@excellent.io>
2018-04-26 15:12:21 -0500

    split logic into handlers
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- components/builder-gateway/cmd/main.go
+++ components/builder-gateway/cmd/main.go
@@ -7,14 +7,11 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/google/uuid"
-
 	pb "github.com/chuckleheads/hurtlocker/components/builder-gateway/api/origins"
-	pbreq "github.com/chuckleheads/hurtlocker/components/builder-gateway/api/origins/request"
-	pbres "github.com/chuckleheads/hurtlocker/components/builder-gateway/api/origins/response"
+	"github.com/chuckleheads/hurtlocker/components/builder-gateway/handlers"
+
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 )
 
 func main() {
@@ -32,7 +29,7 @@ func main() {
 
 func runGRPC(lis net.Listener) {
 	server := grpc.NewServer()
-	pb.RegisterOriginsServer(server, OriginsServer())
+	pb.RegisterOriginsServer(server, handlers.NewOriginsServer())
 
 	log.Printf("gRPC Listening on %s\n", lis.Addr().String())
 	server.Serve(lis)
@@ -48,78 +45,4 @@ func runHTTP(clientAddr string) {
 	}
 	log.Printf("HTTP Listening on %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, mux))
-}
-
-type server struct {
-	origins map[string]pbres.Origin
-}
-
-func OriginsServer() server {
-	return server{
-		origins: make(map[string]pbres.Origin),
-	}
-}
-
-func (s server) GetOrigin(ctx context.Context, req *pbreq.GetOriginReq) (*pbres.OriginResp, error) {
-	log.Println("Getting origin!")
-
-	if req.Id == "" {
-		return nil, grpc.Errorf(codes.InvalidArgument, "Origin ID cannot be empty")
-	}
-
-	origin, exists := s.origins[req.Id]
-	if !exists {
-		return nil, grpc.Errorf(codes.NotFound, "origin not found")
-	}
-
-	ores := pbres.OriginResp{
-		Origin: &origin,
-	}
-
-	log.Println("Origin found!")
-	return &ores, nil
-}
-
-func (s server) CreateOrigin(ctx context.Context, req *pbreq.CreateOriginReq) (*pbres.OriginResp, error) {
-	log.Println("Creating origin!")
-
-	if req.Name == "" {
-		return nil, grpc.Errorf(codes.InvalidArgument, "Origin Name cannot be empty")
-	}
-
-	id := uuid.New().String()
-
-	s.origins[id] = pbres.Origin{
-		Id:   id,
-		Name: req.Name,
-		DefaultPackageVisibility: req.DefaultPackageVisibility,
-	}
-
-	origin := s.origins[id]
-
-	ores := pbres.OriginResp{
-		Origin: &origin,
-	}
-
-	log.Println("Origin created!")
-	return &ores, nil
-}
-
-func (s server) UpdateOrigin(ctx context.Context, req *pbreq.UpdateOriginReq) (*pbres.OriginResp, error) {
-	log.Println("Updating origin!")
-
-	if req.Id == "" {
-		return nil, grpc.Errorf(codes.InvalidArgument, "Origin Id cannot be empty")
-	}
-
-	origin := s.origins[req.Id]
-	origin.DefaultPackageVisibility = req.DefaultPackageVisibility
-	s.origins[req.Id] = origin
-
-	ores := pbres.OriginResp{
-		Origin: &origin,
-	}
-
-	log.Println("Origin updated!")
-	return &ores, nil
 }
---
+++ components/builder-gateway/handlers/origins.go
@@ -0,0 +1,90 @@
+package handlers
+
+import (
+	"context"
+	"log"
+
+	pbreq "github.com/chuckleheads/hurtlocker/components/builder-gateway/api/origins/request"
+	pbres "github.com/chuckleheads/hurtlocker/components/builder-gateway/api/origins/response"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+type server struct {
+	origins map[string]pbres.Origin
+}
+
+func NewOriginsServer() server {
+	return server{
+		origins: make(map[string]pbres.Origin),
+	}
+}
+
+func (s server) GetOrigin(ctx context.Context, req *pbreq.GetOriginReq) (*pbres.OriginResp, error) {
+	log.Println("Getting origin!")
+
+	// What should happen here is use the client that is defined in the "OriginServer" struct above
+	// which will allow you to talk gRPC to the "origin service" that will actually do database things
+
+	if req.Id == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Origin ID cannot be empty")
+	}
+
+	origin, exists := s.origins[req.Id]
+	if !exists {
+		return nil, grpc.Errorf(codes.NotFound, "origin not found")
+	}
+
+	ores := pbres.OriginResp{
+		Origin: &origin,
+	}
+
+	log.Println("Origin found!")
+	return &ores, nil
+}
+
+func (s server) CreateOrigin(ctx context.Context, req *pbreq.CreateOriginReq) (*pbres.OriginResp, error) {
+	log.Println("Creating origin!")
+
+	if req.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Origin Name cannot be empty")
+	}
+
+	id := uuid.New().String()
+
+	s.origins[id] = pbres.Origin{
+		Id:   id,
+		Name: req.Name,
+		DefaultPackageVisibility: req.DefaultPackageVisibility,
+	}
+
+	origin := s.origins[id]
+
+	ores := pbres.OriginResp{
+		Origin: &origin,
+	}
+
+	log.Println("Origin created!")
+	return &ores, nil
+}
+
+func (s server) UpdateOrigin(ctx context.Context, req *pbreq.UpdateOriginReq) (*pbres.OriginResp, error) {
+	log.Println("Updating origin!")
+
+	if req.Id == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Origin Id cannot be empty")
+	}
+
+	origin := s.origins[req.Id]
+	origin.DefaultPackageVisibility = req.DefaultPackageVisibility
+	s.origins[req.Id] = origin
+
+	ores := pbres.OriginResp{
+		Origin: &origin,
+	}
+
+	log.Println("Origin updated!")
+	return &ores, nil
+}
