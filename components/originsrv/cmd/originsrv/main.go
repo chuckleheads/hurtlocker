90b273af80dba267b0eeb75931e46348b7077db9
Josh Black <raskchanky@gmail.com>
2018-05-05 05:46:34 +0000

    attempting to get database access working
    Signed-off-by: Josh Black <raskchanky@gmail.com>

--- components/originsrv/cmd/originsrv/main.go
+++ components/originsrv/cmd/originsrv/main.go
@@ -22,6 +22,9 @@ func main() {
 
 func runGRPC(lis net.Listener) {
 	server := grpc.NewServer()
+	db := server.EstablishConnection()
+	server.RunMigrations(db)
+
 	pb.RegisterOriginsServer(server, srv.NewServer("some_pg_server"))
 
 	log.Printf("gRPC Listening on %s\n", lis.Addr().String())
--- components/originsrv/origins/server/server.go
+++ components/originsrv/origins/server/server.go
@@ -1,12 +1,15 @@
 package server
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 
 	"golang.org/x/net/context"
 
 	"github.com/chuckleheads/hurtlocker/components/originsrv/origins/request"
 	"github.com/chuckleheads/hurtlocker/components/originsrv/origins/response"
+	_ "github.com/lib/pq"
 )
 
 // TODO should be a server handle
@@ -16,6 +19,25 @@ type Server struct {
 
 func NewServer(db string) *Server {
 	return &Server{db}
+}
+
+// This might not be the right place for these two methods, but somehow we need to share the db connection between all these methods. It'd be rad if there was a way to shove it into the Context that all the handlers get. Or maybe there's a more idiomatic way?
+func (srv *Server) EstablishConnection() (*sql.DB, error) {
+	db, err := sql.Open("postgres", "postgresql://hab@localhost:26257/hurtlocker?sslmode=disable")
+
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+
+	return db, err
+}
+
+func (srv *Server) RunMigrations(db *sql.DB) {
+	// This format is not ideal =/
+	log.Println("Running db migrations")
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS origins (id bigserial PRIMARY KEY, name TEXT NOT NULL UNIQUE, default_package_visibility text NOT NULL DEFAULT 'public')"); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func (srv *Server) CreateOrigin(ctx context.Context, req *request.CreateOriginReq) (*response.OriginResp, error) {
