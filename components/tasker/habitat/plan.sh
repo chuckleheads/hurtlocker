a932bbd7c30c9387f8dcb90ddd4688a0ccd9d00e
Elliott Davis <elliott@excellent.io>
2018-07-18 12:05:31 -0500

    Add glibc dep on tasker
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- components/tasker/habitat/plan.sh
+++ components/tasker/habitat/plan.sh
@@ -7,5 +7,5 @@ pkg_upstream_url="https://github.com/chuckleheads/hurtlocker"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
-pkg_deps=(core/hab)
+pkg_deps=(core/hab core/glibc)
 pkg_build_deps=(core/go core/git core/dep)
