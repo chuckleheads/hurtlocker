f8fefa17e699ab15cb546f16f2609d51ae034cf5
Elliott Davis <elliott@excellent.io>
2018-07-19 13:03:41 -0500

    Add dgraph as a graphdb engine into the dev env
    Signed-off-by: Elliott Davis <elliott@excellent.io>

---
+++ components/graph-datastore-server/plan.sh
@@ -0,0 +1,23 @@
+pkg_name=graph-datastore-server
+pkg_origin=chuckleheads
+pkg_version=0.0.1
+pkg_description="dgraph database for builder chuckleheads"
+pkg_upstream_url="https://github.com/chuckleheads/hurtlocker"
+pkg_license=('Apache-2.0')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_deps=(core/dgraph)
+pkg_binds=(
+    [zero]="port"
+)
+pkg_exports=(
+  [port]=port
+)
+pkg_exposes=(port)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
