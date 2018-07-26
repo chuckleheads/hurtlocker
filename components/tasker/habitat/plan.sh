62f5b8013133811664fe2a9e21498e2581daa10b
Elliott Davis <elliott@excellent.io>
2018-07-26 14:40:11 -0500

    adding cacerts
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- components/tasker/habitat/plan.sh
+++ components/tasker/habitat/plan.sh
@@ -7,5 +7,5 @@ pkg_upstream_url="https://github.com/chuckleheads/hurtlocker"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
-pkg_deps=(core/hab core/glibc core/gcc-libs core/libsodium)
+pkg_deps=(core/hab core/glibc core/gcc-libs core/libsodium core/cacerts)
 pkg_build_deps=(core/go core/git core/dep core/pkg-config core/gcc)
