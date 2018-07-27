bb9cfec293b89be770087b9b838b7407c1be3c16
Elliott Davis <elliott@excellent.io>
2018-07-27 15:55:01 -0500

    localize config validation to each command
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- components/tasker/cmd/build.go
+++ components/tasker/cmd/build.go
@@ -3,9 +3,11 @@ package cmd
 import (
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/chuckleheads/hurtlocker/components/tasker/pkg/build"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // buildCmd represents the start command
@@ -20,6 +22,7 @@ var buildCmd = &cobra.Command{
 }
 
 func init() {
+	cobra.OnInitialize(validateBuildConfig)
 	rootCmd.AddCommand(buildCmd)
 }
 
@@ -29,4 +32,22 @@ func tempdir() string {
 		log.Fatal(err)
 	}
 	return dir
+}
+
+// validateConfig ensures any config values we depend on for running a job are present before starting
+func validateBuildConfig() {
+	requiredConfig := []string{"project.origin_name",
+		"project.package_name",
+		"project.name",
+		"project.plan_path",
+		"project.vcs_data",
+		"bldr.private",
+		"bldr.public",
+	}
+	for _, req := range requiredConfig {
+		if !viper.IsSet(req) {
+			log.Fatalln("Missing required config value for: ", req)
+			os.Exit(1)
+		}
+	}
 }
--- components/tasker/cmd/export.go
+++ components/tasker/cmd/export.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"log"
+	"os"
+
 	"github.com/chuckleheads/hurtlocker/components/tasker/pkg/export"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // exportCmd represents the start command
@@ -16,5 +20,19 @@ var exportCmd = &cobra.Command{
 }
 
 func init() {
+	cobra.OnInitialize(validateExportConfig)
 	rootCmd.AddCommand(exportCmd)
+}
+
+// validateConfig ensures any config values we depend on for running a job are present before starting
+func validateExportConfig() {
+	requiredConfig := []string{"ident",
+		"channel",
+	}
+	for _, req := range requiredConfig {
+		if !viper.IsSet(req) {
+			log.Fatalln("Missing required config value for: ", req)
+			os.Exit(1)
+		}
+	}
 }
